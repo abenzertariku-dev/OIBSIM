7137c68a6f20c178e300e49ea381d810f1cff0d3
abenzertariku-dev <abenzertariku@gmail.com>
2026-08-26 09:44:35 +0300

    c

--- BackEnd/src/Routes/Userrout.js
+++ BackEnd/src/Routes/Userrout.js
@@ -2,12 +2,15 @@ import express from"express";
 import {GetUserData, Orderpiza} from "../Controlers/UserCont.js";
 export const UsersRout=express.Router();
 
+
+
 UsersRout.get('/',GetUserData);
 // UsersRout.post('/signup',);
 // UsersRout.get('/pissa',);
 // UsersRout.get('/souse',);
 UsersRout.post('/order',Orderpiza);
 // UsersRout.delete('/order',);
+//UsersRout.get('/myorders')
 
 
 
